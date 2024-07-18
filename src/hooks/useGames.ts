c62a392583d4bb30e69986cb1c3c7541acafa300
Sadia <Sadia.zalmay@gmail.com>
2024-07-18 02:00:33 -0400

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,19 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform{
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game{
     id: number;
     name:string;
     background_image: string;
+    parent_platforms: {
+        map: any;platform: Platform
+}
 }
 
 interface FetchGamesResponse{
@@ -21,10 +30,9 @@ const useGames = () => {
     
         useEffect(() => {
             apiClient
-            .get<FetchGamesResponse>('/games', {signal: controller.signal})
+            .get<FetchGamesResponse>('/games')
             .then((res) => setGames(res.data.results))
             .catch((err) => {
-                if (err instanceof CanceledError) return;
                 setError(err.message)
             });
 
