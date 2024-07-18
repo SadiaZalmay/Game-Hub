022269d6c699c9567ca0f6b0f0194446ad1a4093
Sadia <Sadia.zalmay@gmail.com>
2024-07-18 12:18:52 -0400

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () =>{
-    const {games, error, isLoading} = useGames();
+    const {data, error, isLoading} = useGames();
     const Skeletons = [1,2,3,4,5,6];
     
     return(
@@ -17,7 +17,7 @@ const GameGrid = () =>{
             spacing={20}
         >
         {isLoading && Skeletons.map(Skeletons =><GameCardContainer> <GameCardSkeleton key={Skeletons}/></GameCardContainer>)}
-        {games.map((game) => (
+        {data.map((game) => (
             <GameCardContainer>
                 <GameCard key={game.id} game={game}></GameCard>
             </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform{
     id: number;
@@ -17,35 +15,5 @@ export interface Game{
 }
 }
 
-interface FetchGamesResponse{
-    count: number;
-    results: Game[];
-}
-
-const useGames = () => {
-
-        const [games, setGames] = useState<Game[]>([]);
-        const [error, setError] = useState('');
-        const [isLoading, setLoading] = useState(false);
-    
-        useEffect(() => {
-            const controller = new AbortController();
-        setLoading(true);
-        apiClient
-            .get<FetchGamesResponse>('/games')
-            .then((res) => {
-                setGames(res.data.results)
-                setLoading(false);
-            })
-            
-            .catch((err) => {
-                setError(err.message)
-                setLoading(false);
-            });
-
-        return () => controller.abort();
-        }, []);
-        return{games, error, isLoading};
-    }
-
+const useGames = () => useData<Game>('/games')
 export default useGames;
